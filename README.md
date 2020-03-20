95b8ca0490283f6de12c9732e803b92c86d77789
alyssahellrung <alyssahellrung@gmail.com>
2020-03-20 15:48:26 -0700

    Finished project submission

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # passwordGenerator
 Homework 03
+
+I set up a program to generate a password using user preferences. The html and css were given and were not modified. Some of the JavaScript code was already present, but I had to create the "guts" of the generatePassword function.
+
+I started with the alert, "Let's generate a password!", then prompt the user to choose a password length between 8 and 128 characters. 
+
+I then set up a series of confirm statements asking if the user wants lowercase and/or uppercase letters and/or numbers and/or special characters. These Booleans are stored in the variables "upperCase," "lowerCase," "numbers", and "special".
+
+I set up a validation so that if they do not choose a number between 8 and 128, once the other questions are finished, an alert appears that says the password must be between 8 and 128 characters. Also, if the user does not confirm any of the character types, I alert that they must choose at least one character type.
+
+I then set up four arrays: lowerLetters, upperLetters, numerals, and specialChar. I also set up the variable "password", which is empty to start but will contain the ever-growing string that gets produced down the line.
+
+I merged the arrays (using concat) into every possible combination a user could choose.
+
+I then wrote if and if/else statements for every possible combination. Each statement sets a for loop to keep adding randomly chosen elements from the appropriate array to the password variable until the password is the length the user wanted it to be.
+
+Once the loop finishes, and as long as the user chose a length of 8-128 characters and chose at least one character type, the finished password is returned and the user sees it on their screen. Magic!
--- script.js
+++ script.js
@@ -1,26 +1,170 @@
 // Write a function that takes user input and creates a password
 // based on that input and returns it
 
-// PSEUDOCODE
-// Several prompts
-// --Length (between 8 and 128 characters)
-// --Lowercase letters
-// --Uppercase letters
-// --Numerical characters
-// --Special characters
-// Validate that at least one character type has been selected
-// When all prompts are answered, password should be generated (return?)
-// Alert result
-
-
 
 function generatePassword() {
   /****
    * WRITE YOUR CODE HERE
    */
-  alert("I am in generate password");
 
-  return "sometext";
+  alert("Let's generate a password!");
+
+  var passwordLength = prompt("Choose a password length between 8 and 128 characters");
+  var lowerCase = confirm("Would you like to include lowercase letters?");
+  var upperCase = confirm("Would you like to include uppercase letters?");
+  var numbers = confirm("Would you like to include numerical characters?");
+  var special = confirm("Would you like to include special characters?");
+
+  if ((passwordLength >= 8) && (passwordLength <= 128)) {
+    if (lowerCase || upperCase || numbers || special) {
+
+      var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+      var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "="];
+      var numerals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+      var password = "";
+
+      var megaArray = lowerLetters.concat(upperLetters, specialChar, numerals);
+      var lunArray = lowerLetters.concat(upperLetters, numerals);
+      var lusArray = lowerLetters.concat(upperLetters, specialChar);
+      var lnsArray = lowerLetters.concat(numerals, specialChar);
+      var unsArray = upperLetters.concat(numerals, specialChar);
+      var luArray = lowerLetters.concat(upperLetters);
+      var lnArray = lowerLetters.concat(numerals);
+      var lsArray = lowerLetters.concat(specialChar);
+      var unArray = upperLetters.concat(numerals);
+      var usArray = upperLetters.concat(specialChar);
+      var nsArray = numerals.concat(specialChar);
+      
+      if (lowerCase && upperCase && numbers && special) {
+        for (var i = 0; i < passwordLength; i++) {
+          if (password.length < passwordLength) {
+            password = password + megaArray[Math.floor(Math.random() * 75)];
+          }
+        }
+      } 
+
+      else if (lowerCase && upperCase && numbers) {
+        for (var i = 0; i < passwordLength; i++) {
+          if (password.length < passwordLength) {
+            password = password + lunArray[Math.floor(Math.random() * 62)];
+          }
+        }
+      }
+
+      else if (lowerCase && upperCase && special) {
+        for (var i = 0; i < passwordLength; i++) {
+          if (password.length < passwordLength) {
+            password = password + lusArray[Math.floor(Math.random() * 65)];
+          }
+        }
+      }
+
+      else if (lowerCase && numbers && special) {
+        for (var i = 0; i < passwordLength; i++) {
+          if (password.length < passwordLength) {
+            password = password + lnsArray[Math.floor(Math.random() * 49)];
+          }
+        }
+      }
+
+      else if (upperCase && numbers && special) {
+        for (var i=0; i < passwordLength; i++) {
+          if(password.length < passwordLength) {
+            password = password + unsArray[Math.floor(Math.random() * 49)];
+          }
+        }
+      }
+
+      else if (lowerCase && upperCase) {
+        for (var i=0; i < passwordLength; i++) {
+          if(password.length < passwordLength) {
+            password = password + luArray[Math.floor(Math.random() * 52)];
+          }
+        }
+      }
+
+      else if (lowerCase && numbers) {
+        for (var i=0; i < passwordLength; i++) {
+          if(password.length < passwordLength) {
+            password = password + lnArray[Math.floor(Math.random() * 36)];
+          }
+        }
+      }
+
+      else if (lowerCase && special) {
+        for (var i=0; i < passwordLength; i++) {
+          if(password.length < passwordLength) {
+            password = password + lsArray[Math.floor(Math.random() * 39)];
+          }
+        }
+      }
+
+      else if (upperCase && numbers) {
+        for (var i=0; i < passwordLength; i++) {
+          if(password.length < passwordLength) {
+            password = password + unArray[Math.floor(Math.random() * 36)];
+          }
+        }
+      }
+
+      else if (upperCase && special) {
+        for (var i=0; i < passwordLength; i++) {
+          if(password.length < passwordLength) {
+            password = password + usArray[Math.floor(Math.random() * 39)];
+          }
+        }
+      }
+
+      else if (numbers && special) {
+        for (var i=0; i < passwordLength; i++) {
+          if(password.length < passwordLength) {
+            password = password + nsArray[Math.floor(Math.random() * 23)];
+          }
+        }
+      }
+
+      else if (lowerCase) {
+        for (var i = 0; i < passwordLength; i++) {
+          if (password.length < passwordLength) {
+            password = password + lowerLetters[Math.floor(Math.random() * 26)];
+          }
+        }
+      }
+
+      else if (upperCase) {
+        for (var i = 0; i < passwordLength; i++) {
+          if (password.length < passwordLength) {
+            password = password + upperLetters[Math.floor(Math.random() * 26)];
+          }
+        }
+      }
+
+      else if (numbers) {
+        for (var i = 0; i < passwordLength; i++) {
+          if (password.length < passwordLength) {
+            password = password + numerals[Math.floor(Math.random() * 10)];
+          }
+        }
+      }
+
+      else if (special) {
+        for (var i = 0; i < passwordLength; i++) {
+          if (password.length < passwordLength) {
+            password = password + specialChar[Math.floor(Math.random() * 13)];
+          }
+        }
+      }
+      
+    } else {
+      alert("You must select at least one option.");
+    }
+  } else {
+    alert("Your password must be between 8 and 128 characters.");
+  }
+
+  return password;
+
 }
 
 //////////////////////////////////////////////////////////////
